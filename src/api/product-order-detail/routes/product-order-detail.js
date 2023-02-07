bda1801a5decca24073745c7f3373c668c3d77a1
Dương Chí Thắng <Chithang0210@gmail.com>
2023-02-07 09:56:10 +0700

    push code be

---
+++ src/api/product-order-detail/routes/product-order-detail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * product-order-detail router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::product-order-detail.product-order-detail');
---
+++ src/api/product-order-detail/services/product-order-detail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * product-order-detail service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::product-order-detail.product-order-detail');
