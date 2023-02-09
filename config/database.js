579d118766d2e77f438e5d092c6c5ef0342a1da6
Dương Chí Thắng <Chithang0210@gmail.com>
2023-02-09 10:01:36 +0700

    update database time 2

--- config/database.js
+++ config/database.js
@@ -2,12 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST','containers-us-west-84.railway.app'),
-      port: env.int('DATABASE_PORT','5941'),
-      database: env('DATABASE_NAME','railway'),
-      user: env('DATABASE_USERNAME','root'),
-      password: env('DATABASE_PASSWORD','4GbMOPzmugdTWJquPbTP'),
-      ssl: env.bool('DATABASE_SSL','true'),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: env.bool('DATABASE_SSL'),
     },
   },
 });
